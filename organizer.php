3fc322533b76256c6ba58085089bab60e96aa87e
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-11-21 13:25:14 +0100

    Html done

---
+++ organizer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sierpniowy kalendarz</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1>Organizer:Sierpień</h1>
+        </div>
+        <div>
+            <form action="organizer.php" method="post">
+                <label for="wyd">Zapisz wydarzenie:
+                    <input type="text" name="wyd">
+                </label>
+                <input type="submit" value="OK">
+            </form>
+        </div>
+        <div>
+            <img src="logo2.png" alt="sierpień">
+        </div>
+    </header>
+    <main>
+        <?php
+        //skrypt1
+        ?>
+    </main>
+    <footer>
+        Stronę wykonał: Stanisław Fiedoruk 5J
+    </footer>
+</body>
+
+</html>
